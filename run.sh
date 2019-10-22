406b7e30dd81a1662d0841be74de6c936bdcfdc1
Tongyu <Luoyayu@users.noreply.github.com>
2019-10-22 20:05:09 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+export TELEGRAM_APITOKEN=""
+export GRANTEDIDS=""
+export BOT_DEBUG="false"
+
+export SMART_HOME_API_URL=""
+export SMART_HOME_APITOKEN=""
+
+export ALI_ASR_APPKEY=""
+export ALI_ACCESS_TOKEN=""
+export ASR_API_URL="http://nls-gateway.cn-shanghai.aliyuncs.com/stream/v1/asr"
+export AUDIO_SAMPLING_RATE_ASR="16000"
+
+export ALI_ACCESS_KEYID=""
+export ALI_ACCESS_KEYSECRET=""
+
+./telegram_bot
